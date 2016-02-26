dddcfba6daec3f3879ae2feafc3e505b14c08fb6
Yianni Phillipou <y.phillipou@gmail.com>
2016-02-27 00:18:49 +1100

    Language and user type selectors complete

---
+++ Kamar/app/src/main/java/com/martabak/kamar/activity/SelectLanguageActivity.java
@@ -0,0 +1,92 @@
+package com.martabak.kamar.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.martabak.kamar.R;
+
+import java.util.Locale;
+
+
+public class SelectLanguageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_language);
+
+        final Button englishButton = (Button) findViewById(R.id.language_english);
+        final Button indonesianButton = (Button) findViewById(R.id.language_bahasa);
+
+        englishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.d(SelectLanguageActivity.class.getCanonicalName(), "Previous value:" + getSharedPreferences("userSettings", MODE_PRIVATE).getString("language", "BH"));
+//                SharedPreferences languagePref = getSharedPreferences("userSettings", MODE_PRIVATE);
+//                SharedPreferences.Editor editor = languagePref.edit().
+//                        putString("language", "en");
+//                editor.commit();
+                setLocale("en");
+                displayLanguageToast();
+                Log.d(SelectLanguageActivity.class.getCanonicalName(), "Set locale to English");
+
+                switchActivity();
+            }
+        });
+        indonesianButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.d(SelectLanguageActivity.class.getCanonicalName(), "Previous value:" + getSharedPreferences("userSettings", MODE_PRIVATE).getString("language", "BH"));
+//                SharedPreferences languagePref = getSharedPreferences("userSettings", MODE_PRIVATE);
+//                SharedPreferences.Editor editor = languagePref.edit().
+//                        putString("language", "in");
+//                editor.commit();
+                setLocale("in");
+                displayLanguageToast();
+                Log.d(SelectLanguageActivity.class.getCanonicalName(), "Set locale to Indonesian");
+
+                switchActivity();
+            }
+        });
+    }
+
+    /**
+     * Set the locale of the app.
+     *
+     * @param lang The 2-digit language code.
+     */
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+    }
+
+    /**
+     * Display a toast after language set.
+     */
+    private void displayLanguageToast() {
+        Context context = getApplicationContext();
+        CharSequence text = getResources().getString(R.string.language_set_to);
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Switch activity.
+     */
+    private void switchActivity() {
+        Intent intent = new Intent(this, SelectUserTypeActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ Kamar/app/src/main/java/com/martabak/kamar/activity/SelectUserTypeActivity.java
@@ -0,0 +1,91 @@
+package com.martabak.kamar.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.martabak.kamar.R;
+
+
+public class SelectUserTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user_type);
+
+        final Button guestButton = (Button) findViewById(R.id.guest);
+        final Button staffButton = (Button) findViewById(R.id.staff);
+
+        guestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUserType("guest");
+                displayUserTypeToast();
+                Log.d(SelectUserTypeActivity.class.getCanonicalName(), "Set user to Guest");
+
+                switchActivity();
+            }
+        });
+        staffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUserType("staff");
+                displayUserTypeToast();
+                Log.d(SelectUserTypeActivity.class.getCanonicalName(), "Set user to Staff");
+
+                switchActivity();
+            }
+        });
+    }
+
+    /**
+     * Set the user type.
+     *
+     * @param userType The user type, either "guest" or "staff".
+     */
+    private void setUserType(String userType) {
+        SharedPreferences languagePref = getSharedPreferences("userSettings", MODE_PRIVATE);
+        SharedPreferences.Editor editor = languagePref.edit().
+                putString("userType", userType);
+        editor.commit();
+    }
+
+    /**
+     * Display a toast after user type set.
+     */
+    private void displayUserTypeToast() {
+        Context context = getApplicationContext();
+        String text = getResources().getString(R.string.user_set_to) + " ";
+        String userType = getSharedPreferences("userSettings", MODE_PRIVATE).getString("userType", "none");
+        switch (userType) {
+            case "staff":
+                text += getResources().getString(R.string.staff);
+                break;
+            case "guest":
+                text += getResources().getString(R.string.guest);
+                break;
+            default:
+                text += "none";
+                break;
+        }
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Switch activity.
+     */
+    private void switchActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
