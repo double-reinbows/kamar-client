304defe97e9af7e6cc61a120e9ecc8a35c29c5a7
adarsh <adarsh04@gmail.com>
2016-03-14 11:08:54 +1100

    Made the initial version of the Guest Home Screen.

---
+++ Kamar/app/src/main/java/com/martabak/kamar/activity/GuestHomeActivity.java
@@ -0,0 +1,43 @@
+package com.martabak.kamar.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.martabak.kamar.R;
+
+public class GuestHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_home);
+        final ImageAdapter imgAdapter = new ImageAdapter(this);
+        final GridView gridView = (GridView) findViewById(R.id.guestgridview);
+        gridView.setAdapter(imgAdapter);
+
+        //display feature text on each item click
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(GuestHomeActivity.this, "" + imgAdapter.getItem(position),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+}
+
---
+++ Kamar/app/src/main/java/com/martabak/kamar/activity/ImageAdapter.java
@@ -0,0 +1,87 @@
+package com.martabak.kamar.activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.martabak.kamar.R;
+
+
+/**
+ * Created by adarsh on 5/03/16.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mImages.length;
+    }
+
+    public Object getItem(int position) {
+
+        return mText[position];
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View grid;
+        TextView textView;
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            LayoutInflater inflater = (LayoutInflater) mContext.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            grid = inflater.inflate(R.layout.grid_view, null);
+        } else {
+            grid = (View) convertView;
+        }
+
+        //set image and text of each item in the grid
+        imageView = (ImageView) grid.findViewById(R.id.grid_image);
+        textView = (TextView) grid.findViewById(R.id.grid_text);
+        grid.setLayoutParams(new GridView.LayoutParams(150, 150));
+        textView.setText(mText[position]);
+        imageView.setImageResource(mImages[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        //grid.setPadding(8, 8, 8, 8);
+        return grid;
+    }
+
+    // references to our images
+    private Integer[] mImages = {
+            R.mipmap.ic_myrequests,
+            R.mipmap.ic_restaurant,
+            R.mipmap.ic_housekeeping,
+            R.mipmap.ic_bellboy,
+            R.mipmap.ic_maintenance,
+            R.mipmap.ic_transport,
+            R.mipmap.ic_tellus,
+            R.mipmap.ic_checkout,
+    };
+
+    //Text descriptions
+    private String[] mText = {
+            "MY REQUESTS",
+            "RESTAURANTS",
+            "HOUSEKEEPING",
+            "BELLBOY",
+            "MAINTENANCE",
+            "TRANSPORT",
+            "TELL US",
+            "CHECKOUT"
+    };
+}
