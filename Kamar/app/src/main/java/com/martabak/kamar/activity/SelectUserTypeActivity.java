c206a2bd8d1f2030c7a99d8ea10f59e4d279c766
Reinaldo Gunawan <rhgu@student.unimelb.edu.au>
2016-03-25 00:09:31 +1100

    Sucking dicks

--- Kamar/app/src/main/java/com/martabak/kamar/activity/SelectUserTypeActivity.java
+++ Kamar/app/src/main/java/com/martabak/kamar/activity/SelectUserTypeActivity.java
@@ -3,20 +3,37 @@ package com.martabak.kamar.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.app.Fragment;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.martabak.kamar.R;
 
 
 public class SelectUserTypeActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private StaffTypeFragment staffTypeFragment;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user_type);
 
@@ -37,12 +54,99 @@ public class SelectUserTypeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 setUserType("staff");
-                displayUserTypeToast();
+                //displayUserTypeToast();
+                //Check that the current layout has "fragment_container" id in it
+                if (findViewById(R.id.fragment_container) != null) {
+                    //Check that the fragment isn't being created via the Back button
+                    //because otherwise we could end up with double fragments
+                    if (savedInstanceState != null) {
+                        return;
+                    }
+                }
+
+                //Create a new fragment
+                staffTypeFragment = new StaffTypeFragment();
+
+                //Start up a FragmentManager and then replace "fragment_container" inside the layout
+                //with your fresh StaffTypeFragment
+                getFragmentManager().beginTransaction().
+                        replace(R.id.fragment_container, staffTypeFragment).commit();
+
+                //final Button submitButton = (Button) findViewById(R.id.submit);
+
+
                 Log.d(SelectUserTypeActivity.class.getCanonicalName(), "Set user to Staff");
 
-                switchActivity();
+                //switchActivity();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void sendPassword(View v) {
+        staffTypeFragment.sendPassword(v);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SelectUserType Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.martabak.kamar.activity/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SelectUserType Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.martabak.kamar.activity/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+    public class StaffTypeFragment extends Fragment {
+
+        public StaffTypeFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_staff_login, container, false);
+        }
+        public void sendPassword(View v) {doo ged
+            //staffTypeFragment.sendPassword(v);
+            EditText passwordString   = (EditText) findViewById(R.id.EditTextPassword);
+            Log.v("Password String", passwordString.getText().toString());
+
+        }
     }
 
     /**
