636f2acd643f61fff2d78e9e9bc20cef005028fb
Yianni Phillipou <y.phillipou@gmail.com>
2016-09-06 20:57:32 +1000

    Leaving the constructors for options

--- Kamar/app/src/main/java/com/martabak/kamar/domain/permintaan/Engineering.java
+++ Kamar/app/src/main/java/com/martabak/kamar/domain/permintaan/Engineering.java
@@ -1,12 +1,21 @@
 package com.martabak.kamar.domain.permintaan;
 
+import com.martabak.kamar.domain.options.EngineeringOption;
+
 /**
  * Engineering content type of a {@link Permintaan}.
  */
 public class Engineering extends OptionedContent {
 
+    public Engineering() {
+        super();
+    }
+
+    public Engineering(String message, EngineeringOption option) {
+        super(message, option);
+    }
+
     public String getType() {
         return Permintaan.TYPE_ENGINEERING;
     }
-
 }
