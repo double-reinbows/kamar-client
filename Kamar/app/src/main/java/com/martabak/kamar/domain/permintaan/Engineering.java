822806023b35b1d0937d9a782434e736bd44bc79
Yianni Phillipou <y.phillipou@gmail.com>
2016-09-06 20:42:15 +1000

    Making content options more generic

--- Kamar/app/src/main/java/com/martabak/kamar/domain/permintaan/Engineering.java
+++ Kamar/app/src/main/java/com/martabak/kamar/domain/permintaan/Engineering.java
@@ -1,28 +1,12 @@
 package com.martabak.kamar.domain.permintaan;
 
-import com.martabak.kamar.domain.options.EngineeringOption;
-
 /**
  * Engineering content type of a {@link Permintaan}.
  */
-public class Engineering extends Content {
-
-    /**
-     * The engineering option.
-     */
-    public final EngineeringOption option;
-
-    public Engineering() {
-        super();
-        this.option = null;
-    }
-
-    public Engineering(String message, EngineeringOption option) {
-        super(message);
-        this.option = option;
-    }
+public class Engineering extends OptionedContent {
 
     public String getType() {
         return Permintaan.TYPE_ENGINEERING;
     }
+
 }
--- Kamar/app/src/main/java/com/martabak/kamar/domain/permintaan/Housekeeping.java
+++ Kamar/app/src/main/java/com/martabak/kamar/domain/permintaan/Housekeeping.java
@@ -1,28 +1,12 @@
 package com.martabak.kamar.domain.permintaan;
 
-import com.martabak.kamar.domain.options.HousekeepingOption;
-
 /**
  * Housekeeping content type of a {@link Permintaan}.
  */
-public class Housekeeping extends Content {
-
-    /**
-     * The housekeeping option.
-     */
-    public final HousekeepingOption option;
-
-    public Housekeeping() {
-        super();
-        this.option = null;
-    }
-
-    public Housekeeping(String message, HousekeepingOption option) {
-        super(message);
-        this.option = option;
-    }
+public class Housekeeping extends OptionedContent {
 
     public String getType() {
         return Permintaan.TYPE_HOUSEKEEPING;
     }
+
 }
