98a70554fe7353fe9078b5f93a40d8f50bb60377
Reinaldo Gunawan <rhgu@student.unimelb.edu.au>
2016-11-02 21:55:35 +1100

    Fixed Yianni & Jegz's shit Put the onAttach() activity back into RoomNumberDialog (thx Jegz) activity_housekeeping switch from nonrestaurant to restaurant_actionbar.xml removed comments from StaffPermintaanFragment Engineering is still fucked, can't make requests

--- Kamar/app/src/main/java/com/martabak/kamar/activity/guest/ChangeRoomNumberDialogFragment.java
+++ Kamar/app/src/main/java/com/martabak/kamar/activity/guest/ChangeRoomNumberDialogFragment.java
@@ -4,32 +4,21 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.martabak.kamar.R;
-import com.martabak.kamar.activity.home.SelectUserTypeActivity;
 import com.martabak.kamar.domain.Room;
 import com.martabak.kamar.service.GuestServer;
-import com.martabak.kamar.service.StaffServer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observer;
-import rx.functions.Func1;
 
 /**
  * Change Room Number Fragment.
@@ -55,8 +44,6 @@ public class ChangeRoomNumberDialogFragment extends DialogFragment {
         spinner.setAdapter(adapter);
         spinner.setSelection(0);
 
-        final EditText passwordEditText = (EditText)
-                view.findViewById(R.id.password_edit_text);
 
         final Button confirmButton = (Button) view.findViewById(R.id.change_room_number_confirm);
         final Button cancelButton = (Button) view.findViewById(R.id.change_room_number_cancel);
@@ -67,9 +54,6 @@ public class ChangeRoomNumberDialogFragment extends DialogFragment {
             @Override
             public void onClick(View v) {
                 String roomNumber = roomNumbers.get((int)spinner.getSelectedItemId()).toString();
-                //Not necessary because pwd has been verified by this stage
-                //String password = passwordEditText.getText().toString();
-                //changeRoomNumberWithPassword(roomNumber, password);
                 changeRoomNumber(roomNumber);
 
             }
@@ -89,40 +73,6 @@ public class ChangeRoomNumberDialogFragment extends DialogFragment {
         return dialog;
     }
 
-    /**
-     * Change the room number if the password is correct. This is unused because
-     * password has already been requested.
-     * @param roomNumber The room number.
-     * @param password The password string.
-     */
-    public void changeRoomNumberWithPassword(final String roomNumber, String password) {
-        StaffServer.getInstance(getActivity()).login(password).subscribe(new Observer<Boolean>() {
-            boolean success = false;
-            String reason;
-            @Override public void onCompleted() {
-                Log.v(ChangeRoomNumberDialogFragment.class.getCanonicalName(), "Success: " + success);
-                changeRoomDialogListener.onChangeRoomDialogPositiveClick(
-                        ChangeRoomNumberDialogFragment.this, roomNumber, success, reason);
-                Log.d(ChangeRoomNumberDialogFragment.class.getCanonicalName(), "On completed");
-            }
-            @Override public void onError(Throwable e) {
-                Log.d(ChangeRoomNumberDialogFragment.class.getCanonicalName(), "On error");
-                e.printStackTrace();
-                reason = "Something went wrong";
-                changeRoomDialogListener.onChangeRoomDialogPositiveClick(
-                        ChangeRoomNumberDialogFragment.this, roomNumber, success, reason);
-            }
-            @Override public void onNext(Boolean loginResponse) {
-                Log.v(ChangeRoomNumberDialogFragment.class.getCanonicalName(),
-                        "Login response: " + loginResponse.toString());
-                success = loginResponse;
-                if (!loginResponse) {
-                    reason = "Incorrect Password";
-                }
-
-            }
-        });
-    }
 
     /**
      * Change the room number.
@@ -157,6 +107,11 @@ public class ChangeRoomNumberDialogFragment extends DialogFragment {
         void onChangeRoomDialogPositiveClick(DialogFragment dialog, String roomNumber,
                                              boolean success, String reason);
         void onChangeRoomDialogNegativeClick(DialogFragment dialog);
+    }
+
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        changeRoomDialogListener = (ChangeRoomDialogListener) activity;
     }
 
 }
