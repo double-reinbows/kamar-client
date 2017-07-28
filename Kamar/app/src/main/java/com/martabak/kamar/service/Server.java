db48dd52133d5c3facbc98236d031830f73b4b22
Reinaldo Gunawan <rhgu@student.unimelb.edu.au>
2017-07-28 11:06:26 +0700

    Updated pi@local IP

--- Kamar/app/src/main/java/com/martabak/kamar/service/Server.java
+++ Kamar/app/src/main/java/com/martabak/kamar/service/Server.java
@@ -77,10 +77,10 @@ public abstract class Server {
      */
     public static String getBaseUrl() {
 //        return "http://34.208.159.122:5984/"; // Rei's Amazon EC2
-//        return "http://192.168.1.105:5984/"; // martabak
+        return "http://192.168.10.252:5984/"; // martabak (pi@local)
 //        return "http://192.168.1.6:5984/"; // Yianni's laptop
 //        return "http://34.209.91.208:5984/"; // Yianni's Amazon EC2
-        return "http://54.254.226.224:5984/"; // Adarsh
+//        return "http://54.254.226.224:5984/"; // Adarsh
 //        return "http://110.44.171.30:5984"; // pi@segara from remote
 
     }
